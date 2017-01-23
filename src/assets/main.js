3d8d9876323046dd162a128f1e1bfb0c0562fe55
Vlad <vstolyarov@yahoo.com>
2017-01-23 18:33:16 -0500

    initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
   // your code will go here
+$.ajax({
+    url: 'https://www.codeschool.com/users/vstolyarov.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
+
+  
